c9b685553852a00b8e589fb159caceb05dce83b2
TheOryZ <kadirrturan@gmail.com>
2022-01-23 03:57:47 +0300

    Added IConfiguration and ConfigurationManager titles
    Added IConfiguration and ConfigurationManager titles

--- WebApp_DotNet6/Program.cs
+++ WebApp_DotNet6/Program.cs
@@ -2,6 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+
+//Console.WriteLine(builder.Configuration["Conf:A"]);
 
 var app = builder.Build();
 
@@ -42,4 +45,30 @@ C# 9.0 ile bu durum deðiþmiþtir. Gelen özellik ile main fonksiyonun zoraki imzas
     
     Port : .Net 6 öncesinde uygulama direkt 5001 ve 5000 portlarýnda ayaða kalkýyordu. launchSettings.json altýnda görülebilir. Artýk her uygulamada deðiþiklik göstericek þekilde 
 kurgulanmýþtýr.
+    
+    Services Entegrasyonu : Build edilmeden önce builder üzerinden services eklenmeleri yapýlmaktadýr. Örn: builder.Services. Dikkat edilmesi gereken nokta builder.Build();
+komutundan önce eklenmesi gerekmektedir. Daha sonra build iþleminden sonra eklenen ilgili servies çaðýrýlýp, tanýmlanýr ve kullanmaya hazýr hale getirilir.
+    Örn:
+    builder.Services.AddHttpContextAccessor();
+    var app = builder.Build();
+    app.Services.GetService<IHttpContextAccessor>();
+sýralamasý þeklinde kullanýlýr.
+
+    IConfiguration : Middleware'ler üzerinde konfigurasyonel deðere eriþim ihtiyacý varsa kullanýlýr. app.Configuration
+    ConfigurationManager : Service entegrasyonlarý sürecinde herhangi bir konfigurasyonel bir deðere ihtiyacýmýz varsa bu deðeri bizlere getiren türdür. builder.Configuration[...]
+
+    ConfigurationManager .Net 6^da gelen yeni bir türdür ve .Net 6'dan önceki bir sorunu performanslý bir þekilde çözmemizi saðlar.
+
+--> .Net 6'dan önceki sorun : appsettings.json dýþýnda farklý bir .json dosyasýndan verileri okumak istediðimizde zahmetli bir build iþlemi gerekiyordu. 
+Örnek kod bloðu ;
+    ConfigurationBuilder configurationBuilder = new();
+    configurationBuilder. AddJsonFile("conf.json");
+    IConfigurationRoot configurationRoot configurationBuilder.Build();
+    var sonuC = configurationRoot["Key"];
+.Net 6 bu build maaliyetini ortadan kaldýrmýþtýr. Harici bir .json dosyasý okunmasýnda tekrardan build edilmesinin önüne geçerek performans saðlanmýþtýr.
+
+.Net 6'da örnek kod bloðu ;
+    ConfigurationManager cm = new();
+    cm.AddJsonFile("conf.json");
+    Console.WriteLine(cm["Key"]);
  */
